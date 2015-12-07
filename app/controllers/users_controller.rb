b680a1af8a212f292240ffc2e7abb7e393f11b28
Jie Ma <20069548@mail.wit.ie>
2015-12-07 01:47:21 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,11 @@ class UsersController < ApplicationController
                                   :password, :password_confirmation)
     @user = User.new(secure_params)
     if @user.save
-       flash[:success] = "Welcome to the Twitter App!" 
-       redirect_to @user
+      remember @user
+      flash[:success] = "Welcome to the Twitter App!" 
+      redirect_to @user
     else
-       render 'new'     
+      render 'new'     
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
 
   #get 'static_pages/home'
 
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'users/new'
+  get 'signup' =>  'users#new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
